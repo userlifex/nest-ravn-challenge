8c735e830d447b57f00ab92b7c7f15d1b5158646
Yeyo <juanmdpc@gmail.com>
2021-10-23 21:05:22 -0500

    🗄 local strategy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -11,6 +12,7 @@ import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
     ProductsModule,
     CategoriesModule,
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './local.strategy';
 
-@Module({ controllers: [AuthController], providers: [AuthService] })
+@Module({
+  imports: [PassportModule, UsersModule],
+  controllers: [AuthController],
+  providers: [AuthService, LocalStrategy],
+})
 export class AuthModule {}
