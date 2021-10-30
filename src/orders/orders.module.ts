080365b75aead06adb6b604940cabaf47eb0bdc3
userlifex <eholguinruiz@gmail.com>
2021-10-29 19:04:34 -0500

    src

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AttachmentService } from 'src/attachment/services/attachment.service';
-import { CategoriesService } from 'src/categories/services/categories.service';
-import { SendgridService } from 'src/common/sendgrid/services/sendgrid.service';
-import { ItemsInCartService } from 'src/items-in-cart/services/items-in-cart.service';
-import { PrismaService } from 'src/prisma/services/prisma.service';
-import { ProductsService } from 'src/products/services/products.service';
-import { ShopcartsService } from 'src/shopcarts/services/shopcarts.service';
-import { UsersService } from 'src/users/services/users.service';
+import { AttachmentService } from '../attachment/services/attachment.service';
+import { CategoriesService } from '../categories/services/categories.service';
+import { SendgridService } from '../common/sendgrid/services/sendgrid.service';
+import { ItemsInCartService } from '../items-in-cart/services/items-in-cart.service';
+import { PrismaService } from '../prisma/services/prisma.service';
+import { ProductsService } from '../products/services/products.service';
+import { ShopcartsService } from '../shopcarts/services/shopcarts.service';
+import { UsersService } from '../users/services/users.service';
 import { OrdersController } from './controllers/orders.controller';
 import { OrdersService } from './services/orders.service';
 
